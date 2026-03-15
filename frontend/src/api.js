375f8592347d3cce8623d94d54c1ad5a68e70a10
GowthamKR6672 <gowthamjanaki852696@gmail.com>
2026-03-15 22:00:36 +0530

    Add files via upload

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+const getBaseURL = () => {
+  const envUrl = import.meta.env.VITE_API_BASE_URL;
+  if (envUrl) {
+    return envUrl.endsWith('/api') ? envUrl : `${envUrl}/api`;
+  }
+  return `http://${window.location.hostname}:5001/api`;
+};
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || `http://${window.location.hostname}:5001/api`,
+  baseURL: getBaseURL(),
 });
 
 API.interceptors.request.use((req) => {
